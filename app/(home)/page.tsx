2e51ee5fa5d767dce2e9bde51da9428f4528b317
OM BHARGAV <ombhargav788@gmail.com>
2026-03-25 00:17:28 +0530

    reduced debouncing

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
         })
       );
 
-      await new Promise((resolve) => setTimeout(resolve, 3000));
+      await new Promise((resolve) => setTimeout(resolve, 1000));
 
       setLoaded(true);
     };
